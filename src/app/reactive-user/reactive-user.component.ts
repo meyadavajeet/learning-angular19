3d7b721e989902b41f9c003db6ef24dc0cf66782
Ajeet Atpl <ajeet.yadav@atpl.live>
2026-03-25 05:08:07 +0530

    after reactive and template form with their validation

--- src/app/reactive-user/reactive-user.component.ts
+++ src/app/reactive-user/reactive-user.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-user',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-user.component.html',
   styleUrl: './reactive-user.component.css'
 })
 export class ReactiveUserComponent {
 
+  userForm: FormGroup = new FormGroup({
+    name: new FormControl("",[Validators.required, Validators.minLength(3)]),
+    userName: new FormControl("",[Validators.required, Validators.minLength(4)]),
+    email: new FormControl(""),
+    password: new FormControl("")
+  })
 }
